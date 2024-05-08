9811fbfd6b88a51ebcf9bddc005329e812913e1d
juan pablo <juanpablo.rebolledo53@gmail.com>
2024-05-08 18:06:51 -0500

    terminado el main para operaciones de estaciones

--- main.cpp
+++ main.cpp
@@ -1,84 +1,120 @@
 #include <iostream>
+#include "ListaDeLinea.h"
+//#include <windows.h>
 
 using namespace std;
 void leerEntrada(const char *&resultado);
-<<<<<<< HEAD
-short contarCaracteresEnBuffer();
-=======
+const char* copiarCadena(const char* cadena);
 
->>>>>>> a24674a3c3fb5a47dc98faadba3e5988707b8fd1
+short contarCaracteresEnBuffer();
+ListaDeLinea *l = nullptr;
 int main()
 {
-    cout << "1. agg linea" << endl;
-    cout << "2. rm linea" << endl;
-    cout << "3. consultar linea" << endl;
+    bool ban = 1;
+    while (ban) {
 
-    cout << "Seleccione una opcion" << endl;
-<<<<<<< HEAD
-    short r;
-=======
-    char r;
->>>>>>> a24674a3c3fb5a47dc98faadba3e5988707b8fd1
-    cin >> r;
+        cout << "1. agg linea" << endl;
+        cout << "2. rm linea" << endl;
+        cout << "3. consultar linea" << endl;
 
-    switch (r) {
-    case 1:
-        cout << "Nombre de la linea: ";
-        const char *nombre;
-<<<<<<< HEAD
-        leerEntrada(nombre);
-        cout << nombre;
-        delete[] nombre;
+        cout << "Seleccione una opcion" << endl;
 
+        short r =1;
+        cin >> r;
+
+        system("cls");
+
+        switch (r) {
+        case 1:
+        {
+
+            if (l == nullptr){
+                l = new ListaDeLinea();
+            }
+            cout << "Nombre de la linea: ";
+            const char *nombre;
+
+            leerEntrada(nombre);
+            cout << nombre << endl;
+
+            l->aggLinea(*new NodoLinea(nombre));
+
+            //delete[] nombre; //se deberia encargar el destructor
+        }
         break;
 
-    case 2:
-        break;
+        case 2:
+        {
+            if (l == nullptr){
+                l = new ListaDeLinea();
+            }
+            cout << "Nombre de la linea: ";
+            const char *nombre;
 
-    case 3:
-        break;
+            leerEntrada(nombre);
+            cout << nombre << endl;
 
-=======
-        cin >> nombre;
-        break;
+            //ListaDeLinea l;
+            l->eliminarLinea(*new NodoLinea(nombre));
+            int a;
+            break;
+        }
+        case 3:
+            if(l == nullptr){
+                cout << "No hay lineas";
+            }
 
-    case 2:
-        break;
+            l->imprimirNombresLineas();
+            break;
 
-    case 3:
-        break;
 
->>>>>>> a24674a3c3fb5a47dc98faadba3e5988707b8fd1
-    default:
-        cout << "Seleccione una opcion valida";
-        break;
+        default:
+            cout << "SALIENDO...";
+            ban = false;
+            break;
+        }
+        //return 0;
+
     }
-    return 0;
 }
 
 
 void leerEntrada(const char *&resultado) {
-<<<<<<< HEAD
+
     const short MAX_LENGTH = 100;
     char *entrada = new char[MAX_LENGTH]; // Asignar memoria dinámica
-=======
-    const int MAX_LENGTH = 100;
-    char entrada[MAX_LENGTH];
-    std::cout << "Ingrese una cadena: ";
->>>>>>> a24674a3c3fb5a47dc98faadba3e5988707b8fd1
-    std::cin.getline(entrada, MAX_LENGTH);
+
+    cin.ignore();
+    cin.getline(entrada, MAX_LENGTH);
 
     // Almacenar el resultado en una variable const char *
-    resultado = entrada;
+    //cout << &entrada;
+    resultado = copiarCadena(entrada);
+    cout << &(*entrada) << endl;
+    delete [] entrada;
 }
-<<<<<<< HEAD
+
 
 short contarCaracteresEnBuffer() {
-    std::cin.seekg(0, std::ios::beg);
-    short count = std::cin.gcount();
-    std::cin.clear();
+    cin.seekg(0, std::ios::beg);
+    short count = cin.gcount();
+    cin.clear();
     //std::cin.seekg(0, std::ios::beg);
     return count;
 }
-=======
->>>>>>> a24674a3c3fb5a47dc98faadba3e5988707b8fd1
+
+const char* copiarCadena(const char* cadena) {
+    cout << &(*cadena) << endl;
+    // Calcular la longitud de la cadena de entrada
+    short longitud = contarCaracteresEnBuffer();
+
+    // Crear un nuevo array para almacenar la copia de la cadena
+    char* copia = new char[longitud + 1]; // +1 para el carácter nulo final
+
+    // Copiar los caracteres de la cadena original a la copia
+    for (int i = 0; i <= longitud; ++i) {
+        copia[i] = cadena[i];
+    }
+    //delete[] cadena;
+    return copia;
+}
